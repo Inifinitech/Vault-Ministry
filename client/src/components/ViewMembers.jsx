88b6028cc5e3bcba67edf9e40e4e18f5a57fee87
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-10-21 14:09:17 +0300

    added new route

--- client/src/components/ViewMembers.jsx
+++ client/src/components/ViewMembers.jsx
@@ -13,7 +13,7 @@ function ViewMembers() {
   useEffect(() => {
     const fetchMembers = async () => {
       try {
-        const response = await fetch("http:/10.204.91.2:10000/homemembers");
+        const response = await fetch("http://127.0.0.1:5555/homemembers");
         if (!response.ok) {
           throw new Error("Failed to fetch members");
         }
